991e6a9f7d5034effc9836e9eb3881913e7e0565
Altin_Vitija <altin.vitija@riinvest.net>
2020-04-14 20:51:19 +0200

    I Create LoginScreen and Navigation to HomeScreen

--- app/src/main/java/com/example/loanmanagment/MainActivity.java
+++ app/src/main/java/com/example/loanmanagment/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.loanmanagment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this,"you clicked on text",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
