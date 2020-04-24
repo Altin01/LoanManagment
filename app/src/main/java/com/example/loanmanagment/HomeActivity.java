29d02ee5d7fce9e53f156992dfc2dc17bf5ea674
Altin_Vitija <altin.vitija@riinvest.net>
2020-04-24 17:00:09 +0200

    I created to navigate in settings and in NewLoan

--- app/src/main/java/com/example/loanmanagment/HomeActivity.java
+++ app/src/main/java/com/example/loanmanagment/HomeActivity.java
@@ -2,13 +2,45 @@ package com.example.loanmanagment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
+    ImageView buttonisettings;
+    RelativeLayout buttonnewloan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        buttonisettings = (ImageView) findViewById(R.id.buttonsettings);
+        buttonnewloan=(RelativeLayout) findViewById(R.id.btn_realtivenewloan);
+        buttonisettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HomeActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                Toast.makeText(HomeActivity.this, "you clicked on Settings", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+        buttonnewloan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HomeActivity.this,NewLoanActivity.class);
+                startActivity(intent);
+                Toast.makeText(HomeActivity.this,"you clicked on NewLoan",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
+
 }
--- app/src/main/java/com/example/loanmanagment/SettingsActivity.java
+++ app/src/main/java/com/example/loanmanagment/SettingsActivity.java
@@ -3,12 +3,17 @@ package com.example.loanmanagment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class new_loan extends AppCompatActivity {
-
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_loan);
+        setContentView(R.layout.activity_settings);
+
+
     }
+
+
 }
